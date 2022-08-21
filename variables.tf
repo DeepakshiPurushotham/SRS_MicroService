853b5d0352911df83e2582536b6e0aa2a6cddd61
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-08-21 19:16:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
+variable "access_key" {
+  default = "AKIA342XMEYNLH2UDN4D"
+}
+variable "secret_key" {
+  default = "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"
+}
+
 
 variable "cluster-name" {
   default = "micro_service_eks"
